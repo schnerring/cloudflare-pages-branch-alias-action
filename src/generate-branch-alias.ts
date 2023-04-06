e7ce7155024f4b11a155ec83575f73a3ca2865ff
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2023-04-06 04:39:01 +0200

    Fix eslint errors in generate-branch-alias

--- src/generate-branch-alias.ts
+++ src/generate-branch-alias.ts
@@ -1,49 +1,54 @@
 // Author: Daniel Walsh (https://github.com/WalshyDev)
 // Source: https://community.cloudflare.com/t/algorithm-to-generate-a-preview-dns-subdomain-from-a-branch-name/477633/2
 // License: ?
+//
+// Modified by: Michael Schnerring
 
-const invalidCharsRegex = /[^a-z0-9-]/g;
+const invalidCharsRegex = /[^a-z0-9-]/g
 const maxAliasLength = 28
-const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789"
+const alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
-function generateBranchAlias(branch: string) {
-	const normalised = trim(branch.toLowerCase().replace(invalidCharsRegex, '-'), '-');
+export function generateBranchAlias(branch: string): string {
+  const normalised = trim(
+    branch.toLowerCase().replace(invalidCharsRegex, '-'),
+    '-'
+  )
 
-	if (normalised === '') {
-		return 'branch-' + randAlphaNum(10);
-	}
+  if (normalised === '') {
+    return `branch-${randAlphaNum(10)}`
+  }
 
-	if (normalised.length > maxAliasLength) {
-		return normalised.substring(0, maxAliasLength);
-	}
+  if (normalised.length > maxAliasLength) {
+    return normalised.substring(0, maxAliasLength)
+  }
 
-	return normalised;
+  return normalised
 }
 
 function trim(str: string, char: string): string {
-	while (str.charAt(0) === char) {
-		if (str.length === 1) {
-			return "";
-		}
-		str = str.substring(1);
-	}
+  while (str.startsWith(char)) {
+    if (str.length === 1) {
+      return ''
+    }
+    str = str.substring(1)
+  }
 
-	while (str.charAt(str.length - 1) === char) {
-		if (str.length === 1) {
-			return "";
-		}
-		str = str.substring(0, str.length - 1);
-	}
+  while (str.endsWith(char)) {
+    if (str.length === 1) {
+      return ''
+    }
+    str = str.substring(0, str.length - 1)
+  }
 
-	return str;
+  return str
 }
 
 function randAlphaNum(length: number): string {
-	let result: string = '';
+  let result = ''
 
-	for (let i = 0; i < length; i++) {
-		result += alphanum[Math.floor(Math.random() * alphanum.length)];
-	}
+  for (let i = 0; i < length; i++) {
+    result += alphanum[Math.floor(Math.random() * alphanum.length)]
+  }
 
-	return result;
+  return result
 }
