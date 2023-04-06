2afda8349d971d1a97e0df62a96cb854b0ecb26c
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2023-04-06 04:40:56 +0200

    Implement main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {generateBranchAlias} from './generate-branch-alias'
 
-async function run(): Promise<void> {
+function run(): void {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const gitBranch: string = core.getInput('git-branch')
+    core.debug(`git-branch: ${gitBranch}`)
 
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+
+    const branchAlias = generateBranchAlias(gitBranch)
+
     core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('branch-alias', branchAlias)
+    core.debug(`branch-alias: ${branchAlias}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
