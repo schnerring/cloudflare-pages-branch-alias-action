4a1e640a40a95b4fbad3cfb040482997ae41e654
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2023-04-06 05:05:16 +0200

    Add unit tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,25 +1,30 @@
-import {wait} from '../src/wait'
+import {generateBranchAlias} from '../src/generate-branch-alias'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+test.each([
+  ["test-abc", "test-abc"],
+  ["Test-ABC", "test-abc"],
+  ["Test:abc", "test-abc"],
+  ["-test-abc-", "test-abc"],
+  ["abcdef123456abcdef123456abcdef123456", "abcdef123456abcdef123456abcd"]])(
+    '.generateBranchAlias(%s)',
+    (gitBranch, expected) => {
+  const branchAlias = generateBranchAlias(gitBranch)
+  expect(branchAlias).toBe(expected)
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test("Generate random branch name if input unusable", () => {
+  const input = "-::::-"
+  const branchAlias = generateBranchAlias(input)
+  expect(branchAlias).toMatch(/^branch-[a-z0-9]{10}$/)
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_GIT-BRANCH'] = 'test-abc'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
